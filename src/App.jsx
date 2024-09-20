88f58fe311c73cd1cc87bb9fe338341fb41c41cf
reymaa19 <rcmaala@gmail.com>
2024-09-19 23:02:34 -0500

    Finish adding video player

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import Header from "./components/Header/Header.jsx";
+import videoDetails from './data/video-details.json'
 import "./App.scss";
 
 const App = () => {
+  console.log(videoDetails);
+
   return (
     <>
       <Header />
+      <main>
+        <section className="video">
+          <video className="video__player" controls poster={videoDetails[0].image} src=""></video>
+        </section>
+      </main>
     </>
   );
 };
